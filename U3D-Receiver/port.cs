7440bd6859511864d408719533b06d1a783a616f
Predator-SD <zswdzly@outlook.com>
2016-01-22 12:34:52 +0800

    PC-Serial_Receiver
    On Unity3D

---
+++ U3D-Receiver/port.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class port : MonoBehaviour {
+	public SerialPort sp = new SerialPort("COM3",9600);
+	void Start () {
+		sp.Open();
+	}
+	void Update () {
+		int[] indata;
+		indata = new int[7];
+	    if(sp.IsOpen){
+			for (int i=0; i<7; i++) { indata[i] = sp.ReadByte(); }
+			for(int j=0;j<7;j++){print(indata[j]);}
+		}
+	}
+}
